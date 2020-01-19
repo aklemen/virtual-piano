4093dd91fc517bade9e796bd6e55b179df29304a
Anton Klemen <antonklemen@gmail.com>
2020-01-19 21:57:27 +0100

    Testing with frames list and taking the middle value of box coordinates

--- hand_library/utils/detector_utils.py
+++ hand_library/utils/detector_utils.py
@@ -50,6 +50,12 @@ def load_inference_graph():
     print(">  ====== Hand Inference graph loaded.")
     return detection_graph, sess
 
+def find_middle(number):
+    if number % 2 == 0:
+        return int(number/2)
+    else:
+        return int(number/2 + 0.5)
+
 class Drawer:
     def __init__(self, keys):
         self.p1 = [(0, 0), (0, 0)]
@@ -64,8 +70,12 @@ class Drawer:
         self.num_keys = keys.num_keys
 
         # # Testing
-        # self.previous_center_left = (100, 100)
-        # self.previous_center_right = (keys.im_width-100, 100)
+        self.frames = 5
+        self.middle_index = find_middle(self.frames)
+        self.counter_left = 0
+        self.counter_right = 0
+        self.positions_left = []
+        self.positions_right = []
 
 
     def none_func(self):
@@ -129,28 +139,62 @@ class Drawer:
             cv2.rectangle(image_np, self.p1[i], self.p2[i], (77, 255, 9), 3, 1)
             # cv2.putText(image_np, position, self.p_center[i], cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
-            # distance = 150
-            #
-            # if position == "left":
-            #     if abs(self.previous_center_left[0] - self.p_center[i][0]) > distance or abs(self.previous_center_left[1] - self.p_center[i][1]) > distance:
-            #         t1 = (self.previous_center_left[0]-self.box_size, self.previous_center_left[1]-self.box_size)
-            #         t2 = (self.previous_center_left[0]+self.box_size, self.previous_center_left[1]+self.box_size)
-            #         self.keys.check_coordinates(image_np, t1[0], t1[1], t2[0], t2[1], position)
-            #     else:
-            #         self.keys.check_coordinates(image_np, self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1], position)
-            #         self.previous_center_left = self.p_center[i]
-            # elif position == "right":
-            #     if abs(self.previous_center_right[0] - self.p_center[i][0]) > distance or abs(self.previous_center_right[1] - self.p_center[i][1]) > distance:
-            #         t1 = (self.previous_center_right[0]-self.box_size, self.previous_center_right[1]-self.box_size)
-            #         t2 = (self.previous_center_right[0]+self.box_size, self.previous_center_right[1]+self.box_size)
-            #         self.keys.check_coordinates(image_np, t1[0], t1[1], t2[0], t2[1], position)
-            #     else:
-            #         self.keys.check_coordinates(image_np, self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1], position)
-            #         self.previous_center_right = self.p_center[i]
+            if position == "left":
+                self.positions_left.append([self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1]])
+                self.counter_left += 1
+                if self.counter_left == self.frames:
+                    coordinates = self.positions_left[self.middle_index]
+                    self.keys.check_coordinates(image_np, coordinates[0], coordinates[1], coordinates[2], coordinates[3], position)
+                    self.counter_left = 0
+                    self.positions_left.clear()
+            elif position == "right":
+                self.positions_right.append([self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1]])
+                self.counter_right += 1
+                if self.counter_right == self.frames:
+                    coordinates = self.positions_right[self.middle_index]
+                    print(coordinates)
+                    self.keys.check_coordinates(image_np, coordinates[0], coordinates[1], coordinates[2], coordinates[3], position)
+                    self.counter_right = 0
+                    self.positions_right.clear()
 
-
-            if position != "hands_not_present":
-                self.keys.check_coordinates(image_np, self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1], position)
+    # def draw_box_on_image(self, score_thresh, scores, boxes, im_width, im_height, image_np):
+    #
+    #     position = "hands_not_present"  # This doesn't or shouldn't happen
+    #     for i in range(self.num_hands_detect):
+    #
+    #         if scores[0] > score_thresh and scores[1] > score_thresh:
+    #
+    #             (left, right, top, bottom) = (boxes[i][1] * im_width, boxes[i][3] * im_width,
+    #                                           boxes[i][0] * im_height, boxes[i][2] * im_height)
+    #
+    #             self.p_center[i] = (int(left + (right-left)/2), int(top + (bottom-top)/2))
+    #
+    #             # self.p1[i] = (int(left), int(top))
+    #             # self.p2[i] = (int(right), int(bottom))
+    #
+    #             # Rectangle coordinates
+    #             self.p1[i] = (self.p_center[i][0]-self.box_size, self.p_center[i][1]-self.box_size)
+    #             self.p2[i] = (self.p_center[i][0]+self.box_size, self.p_center[i][1]+self.box_size)
+    #
+    #         # Checking coordinates to activate sounds
+    #         # Directions are swapped, because image is flipped
+    #         if self.p1[0][0] < self.p1[1][0]:
+    #             if i == 0:
+    #                 position = "right"
+    #             elif i == 1:
+    #                 position = "left"
+    #         elif self.p1[0][0] > self.p1[1][0]:
+    #             if i == 0:
+    #                 position = "left"
+    #             elif i == 1:
+    #                 position = "right"
+    #
+    #         # Draw the bounding box and text
+    #         cv2.rectangle(image_np, self.p1[i], self.p2[i], (77, 255, 9), 3, 1)
+    #         # cv2.putText(image_np, position, self.p_center[i], cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+    #
+    #         if position != "hands_not_present":
+    #             self.keys.check_coordinates(image_np, self.p1[i][0], self.p1[i][1], self.p2[i][0], self.p2[i][1], position)
 
 
 
